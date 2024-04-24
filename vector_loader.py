d81036989bfe658abfe42f8140fef2b52d2fe75f
Pankaj Avasthi <pankajavasthi@ymail.com>
2024-04-24 14:27:27 +0530

    sffs

--- vector_loader.py
+++ vector_loader.py
@@ -3,7 +3,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings
 from details import api_key
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
@@ -15,6 +14,5 @@ if __name__=="__main__":
         docs=loader.load()
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         splits = text_splitter.split_documents(docs)
-        vectorstore = FAISS.from_documents(documents=splits, embedding=OpenAIEmbeddings(api_key= api_key))
-        #vectorstore = FAISS.from_documents(documents=splits, embedding=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2"))
+        vectorstore = FAISS.from_documents(documents=splits, embedding=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2"))
         vectorstore.save_local(DB_FAISS_PATH)
